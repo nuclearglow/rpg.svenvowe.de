25546a7a1b43eb66bfd70e3eaa157af2a2b01dfd
Sven Vowe <svenvowe@gmail.com>
2023-11-13 10:35:13 +0100

    chore: fixed group styling

--- src/routes/chars/+page.svelte
+++ src/routes/chars/+page.svelte
@@ -1,15 +1,26 @@
-<h3>Die Abenteurer</h3>
+<div class=" chars">
+  <h3>Die Abenteurer</h3>
 
-<br />
+  <br />
 
-<h4>Duplo die Diebin</h4>
+  <h4>Duplo die Diebin</h4>
 
-<h4>Gnar-Al der Waldläufer</h4>
+  <h4>Gnar-Al der Waldläufer</h4>
 
-<h4>Kilas der Barde</h4>
+  <h4>Kilas der Barde</h4>
 
-<h4>Rohla, die Waldläuferin</h4>
+  <h4>Rohla, die Waldläuferin</h4>
 
-<h4>Skarsnik der Paladin</h4>
+  <h4>Skarsnik der Paladin</h4>
 
-<h4>Xenia, die Hexe</h4>
+  <h4>Xenia, die Hexe</h4>
+</div>
+
+<style>
+  .chars,
+  .chars > h3,
+  .chars > h4 {
+    text-align: center;
+    max-inline-size: 1440px;
+  }
+</style>
