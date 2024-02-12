41018f5a88a8f7fc7f4bc6ff41b3d0c570f270e9
Sven Vowe <svenvowe@gmail.com>
2024-02-12 17:53:38 +0100

    chore: prepared deployment to new server

---
+++ scripts/deploy.sh
@@ -0,0 +1,4 @@
+# deploy files
+rsync --archive --verbose --human-readable --delete $LOCAL_PATH $REMOTE_USERNAME@$REMOTE_SERVER:$REMOTE_PATH
+# restart app
+ssh $REMOTE_SERVER "bash -i -c 'cd $REMOTE_DEPLOYMENTS_PATH; pm2 restart $REMOTE_DEPLOYMENT_NAME'"
